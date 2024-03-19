10f14228825260e9bace2061ad8261f994a0185f
TranHai <haiyeutcx8@gmail.com>
2024-03-19 09:05:21 +0700

    first commit

---
+++ asset/js/script.js
@@ -0,0 +1,20 @@
+const menuLi = document.querySelectorAll('.admin-sidebar-content > ul > li > a')
+const subMenu = document.querySelectorAll('.sub-menu')
+
+for (let index = 0; index < menuLi.length; index++) {
+   menuLi[index].addEventListener('click', (e)=> {
+    e.preventDefault()
+    // menuLi[index].parentNode.querySelector('ul').classList.toggle('active')
+    // console.log(menuLi[index].parentNode.querySelector('ul'))
+
+    for (let i = 0; i < subMenu.length; i++) {
+        subMenu[i].setAttribute("style", "height : 0 px")
+        
+    }
+
+    const submenuHeight = menuLi[index].parentNode.querySelector('ul .sub-menu-items').offsetHeight
+    menuLi[index].parentNode.querySelector('ul').setAttribute("style", "height : " + submenuHeight + "px")
+    // console.log(submenuHeight)
+
+   })
+}
